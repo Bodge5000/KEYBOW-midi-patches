5d8f6beab2d0aa55d7107832783efc584bb12ed3
Bodge5000 <76498195+Bodge5000@users.noreply.github.com>
2023-04-05 23:44:04 +0100

    Update pocket-operator.py

--- pocket-operator.py
+++ pocket-operator.py
@@ -19,7 +19,7 @@ rgb_off = (25, 25, 25)
 start_note = 45
 velocity = 127
 
-note_keys = [
+note_map = [
     45, 52, 57, 64, 47, 53, 59, 65,
     48, 55, 60, 67, 50, 56, 62, 68,
 ]
@@ -33,12 +33,12 @@ while True:
     for key in keys:
         @keybow.on_press(key)
         def press_handler(key):
-            note = note_keys[key.number]
+            note = note_map[key.number]
             key.set_led(*rgb)
             midi.send(NoteOn(note, velocity))
 
         @keybow.on_release(key)
         def release_handler(key):
-            note = note_keys[key.number]
+            note = note_map[key.number]
             key.set_led(*rgb_off)
             midi.send(NoteOff(note, 0))
