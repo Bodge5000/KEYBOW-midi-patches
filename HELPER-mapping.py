a09151ac518764106518e4b5cc05a02efc371031
Bodge5000 <76498195+Bodge5000@users.noreply.github.com>
2023-04-06 00:38:21 +0100

    Create HELPER-mapping.py

---
+++ HELPER-mapping.py
@@ -0,0 +1,35 @@
+###
+# Not an actual patch!
+# Used for mapping logic note layout (starting at bottom left) to actual
+###
+
+def logic_to_actual(mymap):
+    logic = [
+        0, 1, 2, 3, 4, 5, 6, 7,
+        8, 9, 10, 11, 12, 13, 14, 15,
+    ]
+
+    actual = [
+        12, 8, 4, 0, 13, 9, 5, 1,
+        14, 10, 6, 2, 15, 11, 7, 3
+    ]
+    output = [0] * 16
+
+    for i in logic:
+        correct_index = actual.index(i)
+        output[correct_index] = mymap[i]
+    
+    return output
+
+# -------------------------------------------------
+
+# Change this to whatever you want to map
+
+map = [
+    "B2", "C3", "D3", "E3", 
+    "F3", "G3", "A3", "B3",
+    "C4", "D4", "E4", "F4",
+    "G4", "A4", "B4", "C5"
+]
+
+print(logic_to_actual(map))
